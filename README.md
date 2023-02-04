4ba90739bec74322437f024a6f17fbe97f03ab04
Darya Tishchenko QA-41 <tdaniel@bk.ru>
2023-02-04 16:02:08 +0400

    Add README.md

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/bv2kt4u5qsph7pn5?svg=true)](https://ci.appveyor.com/project/Daria-Tiko/2-1-seleniumweb)
--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class CardOrderTest {
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CardOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setup(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    public void shouldSendSuccessfully() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Фролова-Николаева Марина");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79152224455");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
+
+    }
 }
+
